99328196486af106fe563125366420e8142aed76
MouS0926 <moupriya2609@gmail.com>
2023-08-21 17:27:29 +0530

    front

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Router from './component/Router';
+import { Link } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
+
  <Router/>
+
+ <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
     </div>
   );
 }
